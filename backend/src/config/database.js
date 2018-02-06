eb2c95767702ec586af88a86b1a832507956a6c3
Ailton Filho <ailtoncsf@yahoo.com.br>
2018-02-06 17:45:44 -0300

    Aplicando Middleware Redux Promise.

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,4 +2,14 @@ const mongoose = require('mongoose')
 //evitando warning quando o mongoose estiver deprecated
 mongoose.Promise = global.Promise
 
+/**************************************** 
+Processo para startar o MongoDB
+
+    sudo mkdir /data
+    sudo mkdir /data/db
+    sudo chown mongodb:ailton /data/db
+    sudo chmod 775 -R /data/db
+    mongod
+    
+*****************************************/
 module.exports = mongoose.connect('mongodb://localhost/todo')
